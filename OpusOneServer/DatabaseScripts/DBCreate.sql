295e9a3c0f9d62adcbdc7fae0e2628165b817dbe
Aviv Dor Laptop <avivdor.ad@gmail.com>
2023-11-02 07:26:50 +0200

    asd

---
+++ OpusOneServer/DatabaseScripts/DBCreate.sql
@@ -0,0 +1,20 @@
+﻿Use master
+Create Database OpusOneDB
+GO
+
+Use OpusOneDB
+GO
+
+Create Table Users (
+	ID int Identity primary key,
+	Username nvarchar(100) not null,
+	Pwsd nvarchar(100) not null,
+	Email nvarchar(100) not null,
+	CONSTRAINT UC_Username UNIQUE(Username)
+)
+
+GO
+
+INSERT INTO Users Values('kiki123', '12345', 'kiki@gmail.com')
+
+GO
