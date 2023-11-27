77a7926ef30956358e15df097c71979a85577aee
User <User@LAB3-3>
2023-11-27 12:13:50 +0200

    asd

--- OpusOneServer/DatabaseScripts/DBCreate.sql
+++ OpusOneServer/DatabaseScripts/DBCreate.sql
@@ -5,47 +5,64 @@ GO
 Use OpusOneDB;
 GO
 
+
 Create Table Users (
-	ID int Identity PRIMARY KEY,
+	ID int IDENTITY PRIMARY KEY,
 	Username nvarchar(100) NOT NULL,
 	Pwsd nvarchar(100) NOT NULL,
-	Email nvarchar(100) NOT NULL,
+	Email nvarchar(100) NOT NULL, 
 	CONSTRAINT UC_Username UNIQUE(Username)
-);
-
+)
 GO
 
+Create Table Works_Users(
+	ID int IDENTITY PRIMARY KEY,
+	UserID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
+	WorkID int NOT NULL,
+)
+GO
 
 Create Table Posts(
-	ID int Identity PRIMARY KEY,
+	ID int IDENTITY PRIMARY KEY,
 	CreatorID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Title nvarchar(100) NOT NULL,
 	Content nvarchar(1000) ,
-	FilePath nvarchar(250),
+	FilePath nvarchar(255),
 	UploadDateTime datetime NOT NULL,
 	Work int,
 	Composer int,
-);
-
+)
 GO
 
 Create Table Comments(
-	ID int Identity PRIMARY KEY,
+	ID int IDENTITY PRIMARY KEY,
 	PostID int NOT NULL FOREIGN KEY REFERENCES Posts(ID),
 	CreatorID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Content nvarchar(1000) NOT NULL,
 	UploadDateTime datetime NOT NULL,
-);
-
+)
 GO
 
-Create Table Forum(
-	ID int Identity PRIMARY KEY,
+Create Table Forums(
+	ID int IDENTITY PRIMARY KEY,
 	Title nvarchar(255) NOT NULL,
+	ForumDescription nvarchar(255) NOT NULL,
 	CreatorID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
-	CreatedDateTime datetime NOT NULL,
+	CreatedDateTime datetime NOT NULL,	
+	Work int,
+	Composer int,
 )
+GO
 
-INSERT INTO Users Values('kiki123', '12345', 'kiki@gmail.com')
+Create Table ForumComment(
+	ID int IDENTITY PRIMARY KEY,
+	ForumID int NOT NULL FOREIGN KEY REFERENCES Forums(ID),
+	CreatorID int NOT NULL FOREIGN KEY REFERENCES Users(ID),
+	Content nvarchar(1000) NOT NULL,
+	UploadDateTime datetime NOT NULL,
+)
+GO
+
+INSERT INTO Users Values('kiki123', '12345', 'kiki@gmail.com');
 
 GO
