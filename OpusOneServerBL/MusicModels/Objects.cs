4a43b5297e6a6b6ce6cd0d6477ab99812e209cd6
Aviv Dor Laptop <avivdor.ad@gmail.com>
2023-12-11 22:16:03 +0200

    asd

--- OpusOneServerBL/MusicModels/Objects.cs
+++ OpusOneServerBL/MusicModels/Objects.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,30 @@ namespace OpusOneServerBL.Models
 {
     public partial class Composer
     {
+        [NotMapped]
         public DateOnly Birth { get; set; }
+        [NotMapped]
         public DateOnly Death { get; set; }
+        [NotMapped]
         public string Epoch { get; set; }
+        [NotMapped]
         public string Portrait { get; set; }
     }
     public partial class Work
     {
+        [NotMapped]
         public string Subtitle { get; set; }
+        [NotMapped]
         public List<string> SearchTerms { get; set; }
+        [NotMapped]
         public string Popular { get; set; }
+        [NotMapped]
         public string Recommended { get; set; }
+        [NotMapped]
         public string SearchMode { get; set; }
+        [NotMapped]
         public string Catalogue { get; set; }
+        [NotMapped]
         public string Catalogue_Number { get; set; }
     }
 }
